e30c1e5f4fc10339d9fd2b56f69d472caad6afa6
SANTHOSH A <arussanthosh@gmail.com>
2025-05-11 14:51:57 +0530

    Create Variable.js
    Add 1.js

---
+++ Variable.js
@@ -0,0 +1,10 @@
+// Variable
+// Program \\
+var price=100;
+var Prduct="car-Toy";
+var tax=5;
+console.log("The Product "+Prduct +" Price is  "+(price+tax)+ " With GST")
+
+
+//output is
+// The Product car-Toy Price is  105 With GST
