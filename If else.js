4019f64165094108cdffce0fb4cad5f9b3f04085
SANTHOSH A <arussanthosh@gmail.com>
2025-05-11 16:01:54 +0530

    Create If else.js
    Add IF ELSE

---
+++ If else.js
@@ -0,0 +1,33 @@
+var rain=true
+if(rain){
+    console.log("There is Rain outside")
+    
+}
+else{
+    console.log("Noo Rain")
+}
+
+//====================
+
+var homework=false
+if(homework){
+    console.log("I am Done the Job")
+}
+else{
+    console.log("I am Not Done Yet")
+}
+
+//=======================
+var eat=true
+if(eat){
+    console.log("I am Done Yoo")
+}
+else("ah Shit here we go agin")
+
+/*
+
+The OUTPUT is:
+    There is Rain outside
+    I am Not Done Yet
+    I am Done Yoo
+  */
