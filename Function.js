382eb3e5622f4bde6653ed82d347e08da9dce239
SANTHOSH A <arussanthosh@gmail.com>
2025-05-11 15:45:08 +0530

    Create Function.js
    Add Function.js

---
+++ Function.js
@@ -0,0 +1,25 @@
+var name="Devil"
+var lastname =" X"
+var age= "100"
+var location = "Hell"
+var kill= "100"
+
+function Adharcard(){
+    //console.log("The Name is : "+name)
+    console.log("The Name is "+name+" "+lastname)
+    console.log("The "+name+"'s age is "+age)
+    console.log("The "+name+"'s Location is "+location)
+    console.log("The "+name+"'s Kill's is "+kill)
+}
+Adharcard()
+/*
+===================================
+
+The Program OUTPUT IS:
+    The Name is Devil  X
+    The Devil's age is 100
+    The Devil's Location is Hell
+    The Devil's Kill's is 100
+
+===================================
+*/
